1c43493ac29b4da6292d354390b08d3b14cf159f
Sedanah Mashhour <mashhour.h@northeastern.edu>
2023-12-31 10:54:15 -0500

    Revert "updated breakpoints for card"
    This reverts commit 7a4612279dd6809dd5e429041e30c230a0e03436.

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,30 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Sedanah Mashhour'
-}
+  title: 'Sedanah Mashhour',
+  description:
+    'Alex Pate is a product engineer based in Milan, currently building the future of payments at MoonPay. Working somewhere on the boundary between design and code',
+  twitter: {
+    card: 'summary_large_image',
+    creator: '@alexjpate',
+    images: ['/og.png'],
+    title: 'Alex Pate - Product Engineer',
+  },
+  openGraph: {
+    title: 'Alex Pate - Product Engineer',
+    images: [
+      {
+        url: '/og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Alex Pate - Product Engineer',
+      },
+    ],
+    siteName: 'Sedanah Mashhour',
+  },
+  metadataBase: new URL('https://alexjpate.com'),
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
