002a0f7d9238616632ce091c55e3c32486f34a8c
Sedanah Mashhour <mashhour.h@northeastern.edu>
2024-01-28 11:39:43 -0500

    upd exp

--- src/app/page.js
+++ src/app/page.js
@@ -136,7 +136,7 @@ export default function Home() {
 const projects = [
   {
     projectName: "Bionic Hand",
-    description: "Assisted in the software integration for a pioneering project creating an affordable bionic hand for underprivelged children. Contributed expertise in research, literature review, product design, prototyping, and coding, showcasing a comprehensive skill set in driving innovative solutions ",
+    description: "Assisting in the software integration for a pioneering project creating an affordable bionic hand for underprivelged children. Contributing expertise in research, literature review, product design, prototyping, and coding, showcasing a comprehensive skill set in driving innovative solutions ",
     techStack: "Python, SolidWorks, Arduino",
     liveLink: "https://www.giveahandclub.org/projects-7",
   },
