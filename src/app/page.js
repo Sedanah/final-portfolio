2d52d8ff489528857dea4898372aca62fddb8377
Sedanah Mashhour <mashhour.h@northeastern.edu>
2024-02-10 01:54:15 -0500

    slay

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
 import { useState } from "react";
-import { Github, Share, Music4, Linkedin } from "lucide-react";
+import { Github, Share, Music4, Linkedin, BookMarked} from "lucide-react";
 import { useInView } from "react-intersection-observer";
 import FadeIn from "react-fade-in";
+
 
 export default function Home() {
   return (
@@ -139,6 +140,7 @@ const projects = [
     description: "Assisting in the software integration for a pioneering project creating an affordable bionic hand for underprivelged children. Contributing expertise in research, literature review, product design, prototyping, and coding, showcasing a comprehensive skill set in driving innovative solutions. ",
     techStack: "Python, SolidWorks, Arduino",
     liveLink: "https://www.giveahandclub.org/projects-7",
+    BookMarked: href = "/Abstract.pdf"
   },
 
   {
