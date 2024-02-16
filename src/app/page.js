5608214b27c102bb4e7d5f641c6f0832b62bce9e
Sedanah Mashhour <mashhour.h@northeastern.edu>
2024-02-16 14:25:12 -0500

    upd styling

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
 import { useState } from "react";
-import { Github, Share, Linkedin, Link2 } from "lucide-react";
+import { Github, Share, BookMarked, Linkedin, Link2 } from "lucide-react";
 
 export default function Home() {
   return (
@@ -70,7 +70,7 @@ export default function Home() {
   <div className="flex justify-between items-center mb-4">
     <h2 className="text-base font-semibold text-[#64ffda]">Projects</h2>
     <Link href="/projects">
-      <div className="  text-sm text-[#64ffda] font-semibold bg-[#0A1930] hover:bg-[#14342d] transition duration-300 ease-in-out py-1 px-2 rounded animate-bounce">
+      <div className="  text-sm text-[#64ffda] font-semibold bg-[#0A1930] hover:bg-[#14342d] transition duration-300 ease-in-out py-3 px-2 rounded animate-bounce">
         View more projects
       </div> 
     </Link>
@@ -154,7 +154,7 @@ const projects = [
     description:
       "Assisting in the software integration for a pioneering project creating an affordable bionic hand for underprivelged children. Contributing expertise in research, literature review, product design, prototyping, and coding. ",
     techStack: "Python, SolidWorks, Arduino",
-    liveLink: "https://www.giveahandclub.org/projects-7",
+    liveLink: "/Abstract.HM.pdf",
   },
 
   {
@@ -178,7 +178,7 @@ export const ProjectCard = ({
   const stack = techStack.split(", ");
 
   return (
-    <div className="transition ease-in-out hover:-translate-y-1 flex px-5 py-8 flex-col gap-2 justify-between bg-[#132546] hover:bg-[#134e4a6e] text-white rounded">
+    <div className="transition ease-in-out hover:-translate-y-1 flex px-4 sm:px-5 py-8 flex-col gap-2 justify-between bg-[#112240] hover:bg-[#14342d] text-white rounded">
       <div className="flex flex-row items-center justify-between">
         <h3 className="text-xl font-bold text-white/90">{projectName}</h3>
         {githubLink && (
@@ -191,7 +191,12 @@ export const ProjectCard = ({
         {liveLink && (
           <div className="flex items-center">
             <a href={liveLink} target="_blank" rel="noopener noreferrer">
-              <Share className="w-6 h-6" />
+              {/* Use the Book icon for the "Bionic Hand" project */}
+              {liveLink === "/Abstract.HM.pdf" ? (
+                <BookMarked className="w-6 h-6" />
+              ) : (
+                <Share className="w-6 h-6" />
+              )}
             </a>
           </div>
         )}
@@ -205,7 +210,6 @@ export const ProjectCard = ({
         ))}
       </div>
     </div>
-    // RAWR
   );
 };
 
@@ -269,7 +273,7 @@ const experiences = [
     company: "NU Sci",
     role: " Software Engineer",
     description:
-      "Involved in full-stack web development divided in 5 phases of a custom guided project. Working with various concepts",
+      "Involved in full-stack web development divided in 5 phases of a custom guided project.",
   },
 
   {
